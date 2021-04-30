7eeabbf026d58721bf3a1074ab841e3fdde3b089
Ian <iansedano@gmail.com>
2021-04-30 10:53:15 +0200

    ammendment to library!

--- src/lib/d.js
+++ src/lib/d.js
@@ -4,4 +4,5 @@ function c(tag, content, cssClass = None) {
     element = d.createElement(tag)
     element.innerText = content
     if (cssClass) element.classList.add(cssClass)
+    return element
 }
