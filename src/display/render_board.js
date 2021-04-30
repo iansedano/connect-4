d4743577ab03f1b3533ad654a750cfccfa44e613
Ian <iansedano@gmail.com>
2021-04-30 12:39:59 +0200

    further design of board render function

--- src/display/render_board.js
+++ src/display/render_board.js
@@ -1,9 +1,17 @@
 renderBoard(board){
     let display = null
-    board.forEach(row => {
+    board.forEach((row, i) => {
 
-        row.forEach(cell => {
-            
+        row.forEach((value, j) => {
+            createCell(value, i, j)
         })
+    })
+}
+
+
+function createCell(value, row, col){
+    cell = c("span", value, cell);
+    cell.addEventListener("click", e => {
+        updateBoard(row, col)
     })
 }
