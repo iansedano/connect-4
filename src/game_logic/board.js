62123f25f3d24ad03336d5eb3131a62b5300fd89
Ian <iansedano@gmail.com>
2021-05-01 10:28:17 +0200

    commenting board.js

--- src/game_logic/board.js
+++ src/game_logic/board.js
@@ -1,7 +1,16 @@
-const boardSize = 4
+// ==========================
+// ==== Global Variables ====
+const boardSize = 15
 const board = buildBoard(boardSize)
 let turn = 1
+// ==== Global Variables ====
+// ==========================
 
+/**
+ * Create a 2D array filled with 0s representing an empty board
+ * @param {number} boardSize 
+ * @returns {number[][]} representing the board
+ */
 function buildBoard(boardSize) {
     const board = []
 
@@ -15,6 +24,11 @@ function buildBoard(boardSize) {
     return board
 }
 
+/**
+ * Places a stone at given coordinates
+ * @param {number} row 
+ * @param {number} col 
+ */
 function updateBoard(row, col) {
     const currentValue = board[row][col]
 
@@ -25,6 +39,9 @@ function updateBoard(row, col) {
     updateTurn()
 }
 
+/**
+ * Takes the global turn value and flip flops it between 1 or 2
+ */
 function updateTurn() {
     if (turn == 1) {
         turn = 2
@@ -35,42 +52,55 @@ function updateTurn() {
     }
 }
 
+/**
+ * Check if a given position is part of a chain of 4 of the same player
+ * @param {number} row 
+ * @param {number} col 
+ * @returns 
+ */
 function isConnectFour(row, col){
     // setting up cardinal directions
-    console.log("\n\nis connect 4?", row, col)
-    const N = [-1,0]
-    const NE = [-1,1]
-    const E = [0,1]
-    const SE = [1,1]
-    const S = [1,0]
-    const SW = [1,-1]
-    const W = [0,-1]
-    const NW = [1,-1]
+    const N  = [-1, 0]
+    const NE = [-1, 1]
+    const E  = [ 0, 1]
+    const SE = [ 1, 1]
+    const S  = [ 1, 0]
+    const SW = [ 1,-1]
+    const W  = [ 0,-1]
+    const NW = [ 1,-1]
 
     const axes = [[N,S], [NE, SW], [E,W], [SE, NW]]
 
     output = 0
     axes.forEach(axis => {
-        console.log(axis)
         const directionOne = howFarExtends(axis[0], [row,col])
         const directionTwo = howFarExtends(axis[1], [row,col])
         if (directionOne + directionTwo + 1 > 3) {
             console.log("winner!!")
             output = board[row][col]
         }
-
     })
-    console.log("output", output)
     return output
 }
 
+/**
+ * Takes a direction vector and a position and recursively determines how many
+ * same pieces in that direction there are.
+ * @param {number[]} direction 
+ * @param {number[]} position 
+ * @param {number} count 
+ * @returns {number} the number of pieces of same type in that direction
+ */
 function howFarExtends(direction, position, count = 0) {
     const currentValue = board[position[0]][position[1]]
     const newPosition = step(direction, position)
+
+    // Check if the 
     const isOutOfRange = newPosition[0] >= boardSize ||
-                        newPosition[1] >= boardSize ||
-                        newPosition[0] < 0 ||
-                        newPosition[1] < 0
+                         newPosition[1] >= boardSize ||
+                         newPosition[0] <  0 ||
+                         newPosition[1] <  0
+
     if (isOutOfRange){
         return count
     }
@@ -80,6 +110,12 @@ function howFarExtends(direction, position, count = 0) {
     } else return count
 }
 
+/**
+ * 
+ * @param {number[]} direction 
+ * @param {number[]} position 
+ * @returns {number[]}
+ */
 function step(direction, position) {
     return [position[0] + direction[0], position[1] + direction[1]] 
 }
--- src/index.js
+++ src/index.js
@@ -7,15 +7,19 @@ root.appendChild(boardElement)
 function handleBoardClick(row, col){
     try {
         updateBoard(row, col)
+        // Build entire board again.
         boardElement = buildBoardElement(board)
+        // Clear current board
         root.innerHTML = ""
         root.appendChild(boardElement)
+
         const winner = isConnectFour(row, col)
         if (winner === 1) {
             console.log("player 1 wins")
         } else if (winner === 2) {
             console.log("player 2 wins")
         }
+
     } catch (e) {
         console.error(e)
     }
